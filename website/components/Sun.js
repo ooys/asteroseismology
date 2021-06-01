a2b5115d466cf0f9bf8a0c939ef5b52ed450d736
Yihong Song <1834395537@qq.com>
2021-06-01 11:48:23 -0400

    Painted Sun Surface

--- website/components/Sun.js
+++ website/components/Sun.js
@@ -4,19 +4,6 @@ import * as THREE from "three";
 
 function Sun() {
     useEffect(() => {
-        const _VS = `
-        uniform float u_time;
-            void main(){
-                gl_Position = projectionMatrix * modelViewMatrix* vec4(position,1.0);
-            }
-        `;
-        const _FS = `
-        uniform float u_time;
-            void main(){
-                gl_FragColor = vec4(0.0,1.0,0.5,1.0);
-            }
-        `;
-
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(
             75,
@@ -24,28 +11,32 @@ function Sun() {
             0.1,
             1000
         );
-
         const renderer = new THREE.WebGLRenderer({
             canvas: document.querySelector("#sun_canvas"),
         });
+
+        // controls = new THREE.OrbitControls(camera, renderer.domElement);
+
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
         camera.position.setZ(2);
         camera.position.setX(0);
         renderer.render(scene, camera);
 
-        const geometry = new THREE.SphereBufferGeometry(1, 50, 50);
-
-        const uniforms = {};
-        uniforms.u_time = { value: 0.0 };
-
-        const material2 = new THREE.ShaderMaterial({
-            vertexShader: _VS,
-            fragmentShader: _FS,
+        const geometry = new THREE.SphereBufferGeometry(1, 100, 100);
+        // const material = new THREE.MeshBasicMaterial({
+        //     color: 0xffffff,
+        //     wireframe: true,
+        // });
+        const sunTexture = new THREE.TextureLoader().load("/images/sun.jpeg");
+        const material2 = new THREE.MeshBasicMaterial({
+            map: sunTexture,
+            wireframe: false,
         });
-        const torus = new THREE.Mesh(geometry, material2);
 
-        scene.add(torus);
+        const sun = new THREE.Mesh(geometry, material2);
+
+        scene.add(sun);
         const pointLight = new THREE.PointLight(0xffffff);
         pointLight.position.set(5, 5, 5);
 
@@ -55,13 +46,15 @@ function Sun() {
         function animate() {
             requestAnimationFrame(animate);
 
-            torus.rotation.x += 0.01;
-            torus.rotation.y += 0.005;
-            torus.rotation.z += 0.01;
-
+            sun.rotation.x += 0.001;
+            sun.rotation.y += 0.002;
+            sun.rotation.z += 0.001;
+            // controls.update();
             renderer.render(scene, camera);
         }
         animate();
+
+        //renderer.render(scene, camera);
     });
 
     return (
--- website/pages/index.js
+++ website/pages/index.js
@@ -5,6 +5,7 @@ function Home() {
     return (
         <>
             <div>Asteroseismology</div>
+            <Sun />
         </>
     );
 }
